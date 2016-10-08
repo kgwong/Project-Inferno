32188462ca890698b81d23bacbb8a51bf1c06ac6
RyanYLiu <ryanliu967@gmail.com>
2016-10-07 17:36:08 -0700

    add my test scene
    add Character
    
    new scene commit
    
    test another pull request
    
    Add normal images
    
    Remove attack type
    
    Add SFW images and most state transitions
    
    Fix Jump animation

---
+++ Assets/Character/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+﻿public static class Constants
+{
+    public static int STATE_IDLE { get { return 0; } }
+    public static int STATE_MOVE_LEFT { get { return 1; } }
+    public static int STATE_MOVE_RIGHT { get { return 2; } }
+    public static int STATE_ROLL { get { return 3; } }
+    public static int STATE_MID_ATTACK { get { return 4; } }
+    public static int STATE_MID_COMBO { get { return 5; } }
+    public static int STATE_JUMP { get { return 6; } }
+
+	public static int BASE_MOVE_SPEED { get { return 100; } }
+	public static int BASE_JUMP_SPEED { get { return 250; } }
+}

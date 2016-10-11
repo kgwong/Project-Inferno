b0dffaa626f30fabdd440bcfe5b43d1cffc205a7
PaxtonWong <wongpn@uci.edu>
2016-10-11 14:58:24 -0700

    Add basic platform and character

--- Assets/Character/Scripts/Movement.cs
+++ Assets/Character/Scripts/Movement.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
 
+    public float movespeed;
+    public float jumpheight;
+    private float moveX;
+
+    Rigidbody2D rb;
 	// Use this for initialization
 	void Start () {
-	
+
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        moveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveX * movespeed, rb.velocity.y);
+	    
 	}
 }

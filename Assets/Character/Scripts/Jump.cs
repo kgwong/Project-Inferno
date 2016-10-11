2b60af36ca2d0290a4ba7f62494f210bb9d74601
PaxtonWong <wongpn@uci.edu>
2016-10-11 16:40:29 -0700

    Add jump, basic character, side movement, basic platform

---
+++ Assets/Character/Scripts/Jump.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+    
+    public float moveSpeedY;
+    private bool inJump; 
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (!inJump)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if (rb.velocity.y == 0f)
+                {
+                    inJump = true;
+                }
+                
+
+            }
+        }
+	}
+
+    void FixedUpdate()
+    {
+        if (inJump)
+        {
+            rb.velocity = (new Vector2(rb.velocity.x, moveSpeedY));
+            inJump = false;
+        }
+    }
+}
---
+++ Assets/Character/Scripts/Run.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Run : MonoBehaviour {
+
+    public float moveSpeedX;
+    private float moveDirX;
+
+    Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirX = Input.GetAxis("Horizontal");
+    }
+	void FixedUpdate () {
+       
+        rb.velocity = new Vector2(moveDirX * moveSpeedX, rb.velocity.y);
+	    
+	}
+}

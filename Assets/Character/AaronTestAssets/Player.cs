87db251305e9e3f12020eb664d9729c8b6457aaa
Aaron Ching <saltie.crackers@gmail.com>
2016-10-18 01:16:23 -0700

    test commit

---
+++ Assets/Character/AaronTestAssets/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class Player : MonoBehaviour {
+
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+
+        animator = GetComponent<Animator>();
+       // if (animator.)
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

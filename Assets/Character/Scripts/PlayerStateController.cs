7d50294405b1a336e7c10ee503f3126e0e7b4c9c
Christian <christian.navasca@gmail.com>
2016-10-25 14:46:20 -0700

    Add state machine and animation controller

--- Assets/Character/Scripts/PlayerStateController.cs
+++ Assets/Character/Scripts/PlayerStateController.cs
@@ -4,41 +4,45 @@ public class PlayerStateController : MonoBehaviour
 {
 	public Animator animator;
 
-    private SpriteRenderer _spriteRenderer;
+	private SpriteRenderer _spriteRenderer;
 	private bool _facingRight;
-    float _distToGround;
+	float _distToGround;
 
 	void Start ()
 	{
 		animator = this.GetComponent<Animator>();
 		animator.SetInteger("state", Constants.STATE_IDLE);
-        _facingRight = true;
-        _spriteRenderer = this.GetComponent<SpriteRenderer>();
-        _distToGround = this.GetComponent<Collider2D>().bounds.extents.y;
+		_facingRight = true;
+		_spriteRenderer = this.GetComponent<SpriteRenderer>();
+		_distToGround = this.GetComponent<Collider2D>().bounds.extents.y;
 	}
 
-    void Update()
-    {
-        if (InState(Constants.STATE_IDLE))
-        {
-            IdleStateUpdate();
-        }
-        else if (InState(Constants.STATE_MID_ATTACK))
-        {
-            MidAttackStateUpdate();
-        }
-        else if (InState(Constants.STATE_MOVE_LEFT) || InState(Constants.STATE_MOVE_RIGHT))
-        {
-            MoveStateUpdate();
-        }
-        else if (InState(Constants.STATE_ROLL))
-        {
-            RollStateUpdate();
-        }
+	void Update()
+	{
+		if (InState(Constants.STATE_IDLE))
+		{
+			IdleStateUpdate();
+		}
+		else if (InState(Constants.STATE_MID_ATTACK))
+		{
+			MidAttackStateUpdate();
+		}
+		else if (InState(Constants.STATE_MOVE_LEFT) || InState(Constants.STATE_MOVE_RIGHT))
+		{
+			MoveStateUpdate();
+		}
+		else if (InState(Constants.STATE_ROLL))
+		{
+			RollStateUpdate();
+		}
 		else if (InState(Constants.STATE_JUMP))
 		{
-            JumpStateUpdate();
-        }
+			JumpStateUpdate();
+		}
+		else if (FinishedCurrentAnimation())
+		{
+			animator.SetInteger("state", Constants.STATE_IDLE);
+		}
 	}
 
 	public bool InState(int state)
@@ -65,95 +69,100 @@ public class PlayerStateController : MonoBehaviour
 	}
 
 	bool PressedJump()
-    {
-        return Input.GetKey(KeyCode.B);
-    }
+	{
+		return Input.GetKey(KeyCode.B);
+	}
 	bool PressedMoveLeft()
-    {
-        return Input.GetKeyDown(KeyCode.LeftArrow);
-    }
+	{
+		return Input.GetKeyDown(KeyCode.LeftArrow);
+	}
 	bool PressedMoveRight()
-    {
-        return Input.GetKeyDown(KeyCode.RightArrow);
-    }
+	{
+		return Input.GetKeyDown(KeyCode.RightArrow);
+	}
 
 	void IdleStateUpdate()
-    {
+	{
 		if (!IsGrounded())
-        {
-            return;
-        }
+		{
+			return;
+		}
 
 		if (PressedRoll())
 		{
 			ChangeState(Constants.STATE_ROLL);
 		}
 		else if (PressedJump())
-        {
-            ChangeState(Constants.STATE_JUMP);
-        }
+		{
+			ChangeState(Constants.STATE_JUMP);
+		}
 		else if (PressedMoveLeft())
-        {
-            // default face right
-            ChangeState(Constants.STATE_MOVE_LEFT);
+		{
+			// default face right
+			ChangeState(Constants.STATE_MOVE_LEFT);
 			_spriteRenderer.flipX = true;
-            _facingRight = false;
-        }
+			_facingRight = false;
+		}
 		else if (PressedMoveRight())
-        {
-            // default face right
-            ChangeState(Constants.STATE_MOVE_RIGHT);
-            _spriteRenderer.flipX = false;
-            _facingRight = true;
-        }
+		{
+			// default face right
+			ChangeState(Constants.STATE_MOVE_RIGHT);
+			_spriteRenderer.flipX = false;
+			_facingRight = true;
+		}
 		else if (PressedMidAttack())
 		{
 			ChangeState(Constants.STATE_MID_ATTACK);
-            _spriteRenderer.flipX = (_facingRight) ? false : true;
+			_spriteRenderer.flipX = (_facingRight) ? false : true;
 		}
-    }
+	}
 
 	void MidAttackStateUpdate()
-    {
-        if (PressedMidAttack())
-        {
-            ChangeState(Constants.STATE_MID_COMBO);
-            _spriteRenderer.flipX = (_facingRight) ? false : true;
-        }
-        else
+	{
+		if (!FinishedCurrentAnimation() && PressedMidAttack())
+		{
+			ChangeState(Constants.STATE_MID_COMBO);
+		}
+		else if (FinishedCurrentAnimation())
         {
             ChangeState(Constants.STATE_IDLE);
         }
-    }
+	}
 
 	void MoveStateUpdate()
-    {
+	{
 		if (!_facingRight && !Input.GetKey(KeyCode.LeftArrow))
-        {
-            ChangeState(Constants.STATE_IDLE);
-        }
+		{
+			ChangeState(Constants.STATE_IDLE);
+		}
 
 		if (_facingRight && !Input.GetKey(KeyCode.RightArrow))
-        {
-            ChangeState(Constants.STATE_IDLE);
-        }
-    }
+		{
+			ChangeState(Constants.STATE_IDLE);
+		}
+	}
 
 	void RollStateUpdate()
-    {
-        ChangeState(Constants.STATE_IDLE);
-    }
+	{
+		ChangeState(Constants.STATE_IDLE);
+	}
 
 	void JumpStateUpdate()
-    {
-        ChangeState(Constants.STATE_IDLE);
-    }
+	{
+		ChangeState(Constants.STATE_IDLE);
+	}
 
 	bool IsGrounded()
+	{
+		float pad = .1f;
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, _distToGround + pad);
+		bool result = hit.collider != null;
+		return result;
+	}
+
+	bool FinishedCurrentAnimation()
     {
-        float pad = .1f;
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, -Vector2.up, _distToGround + pad);
-        bool result = hit.collider != null;
-        return result;
+        // 1.15 = played once before and am 15% through this loop
+        return (int)animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1;
     }
 }

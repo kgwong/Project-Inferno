ed6fe3e57c38444ae2b57589570eb70114faa670
Aaron Ching <saltie.crackers@gmail.com>
2016-10-28 18:03:15 -0700

    add roll function

--- Assets/Character/AaronTestAssets/PlayerMovement.cs
+++ Assets/Character/AaronTestAssets/PlayerMovement.cs
@@ -6,79 +6,108 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 
-    public float speed = 2f;
-    public float jump = 8f;
+    public float move_speed = 2f;
+    public float jump_power = 8f;
+    public float roll_speed = 4f;
+    public float roll_time = 0.5f;
+
+
     private Animator animator;
     private Rigidbody2D rigidbody;
-    private bool isFacingRight = true;
-	// Use this for initialization
+    private int directionFacing = 1;
+    //private ArrayList lockStates = new ArrayList();
+    private AnimatorStateInfo stateInfo;
+    private Vector3 scale;
+    private float timer;
+	
+
 	void Start () {
 
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody2D>();
-
-        // if (animator.)
+       // lockStates.Add();
+        stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        scale = transform.localScale;
 
     }
 	
-	// Update is called once per frame
 	void FixedUpdate () {
-
-        float horizontal = (Input.GetAxis("Horizontal"));
-
-        //position based
-        transform.position += new Vector3(horizontal, 0, 0) * speed * Time.deltaTime;
-
-        //add force
-        //rigidbody.AddForce(new Vector2(horizontal * speed, 0), ForceMode2D.Force);
-
-        if(animator.GetBool("OnGround"))
+        if (!animator.GetBool("Rolling"))
         {
-            if (horizontal != 0)
-            {
 
-                animator.Play("Walk");
-                Flip(horizontal);
+            float horizontal = (Input.GetAxis("Horizontal"));
+
+            //position based
+            transform.position += new Vector3(horizontal, 0, 0) * move_speed * Time.deltaTime;
+
+            //add force
+            //rigidbody.AddForce(new Vector2(horizontal * speed, 0), ForceMode2D.Force);
+
+                if (animator.GetBool("OnGround"))
+                {
+                    if (horizontal != 0)
+                    {
+                        animator.Play("Walk");
+                        Flip(horizontal);
+                    }
+
+                    else
+                    {
+                        animator.Play("Idle");
+                    }
+
+                    if (Input.GetKeyDown("space"))
+                    {
+                        animator.SetBool("OnGround", false);
+                        animator.Play("Jump");
+                        rigidbody.AddForce(new Vector2(0, jump_power), ForceMode2D.Impulse);
+                    }
+
+                    if (Input.GetKeyDown(KeyCode.X))
+                    {
+                        animator.SetBool("OnGround", false);
+                        animator.SetBool("Rolling", true);
+                        timer = Time.time;
+                    }
+
+                }
+            }
+        else 
+        {
+            float timePassed = Time.time-timer;
+            print(timePassed);
+            if (timePassed < roll_time)
+            {
+                animator.Play("Roll");
+                Flip(directionFacing);
+                rigidbody.AddForce(new Vector2(roll_speed * directionFacing, 0), ForceMode2D.Impulse);
+                print("adding force");
             }
             else
             {
-                animator.Play("Idle");
-            }
-            if (Input.GetKeyDown("space"))
-            {
-                animator.SetBool("OnGround", false);
-                animator.Play("Jump");
-                rigidbody.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+                animator.SetBool("OnGround", true);
+                animator.SetBool("Rolling", false);
             }
         }
-
-
     }
 
     void Flip(float horizontal)
     {
-        if ((isFacingRight && horizontal < 0) || (!isFacingRight && horizontal > 0))
+        if ((directionFacing == 1 && horizontal < 0) || (directionFacing == -1 && horizontal > 0))
         {
-            Vector3 scale = transform.localScale;
             scale.x *= -1;
             transform.localScale = scale;
 
-            if (horizontal > 0)
-            {
-                isFacingRight = true;
-            }
+            directionFacing = (int)(horizontal / Mathf.Abs(horizontal));
 
-            if (horizontal < 0)
-            {
-                isFacingRight = false;
-            }
         }   
     }
 
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-
+        //maybe add a bit of space above the ground to be considered OnGround
+        //attempting to jump slightly before you actually hit the ground and failing feels bad
         if(coll.gameObject.tag == "Platform")
         {
             animator.SetBool("OnGround", true);

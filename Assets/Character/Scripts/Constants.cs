8c24bec91fd3c64f8eb488464759b9ca691a314f
Christian <christian.navasca@gmail.com>
2016-11-09 17:17:42 -0800

    Add switch statement, clean up combo handling

--- Assets/Character/Scripts/Constants.cs
+++ Assets/Character/Scripts/Constants.cs
@@ -1,13 +1,16 @@
 ﻿public static class Constants
 {
-    public static int STATE_IDLE { get { return 0; } }
-    public static int STATE_MOVE_LEFT { get { return 1; } }
-    public static int STATE_MOVE_RIGHT { get { return 2; } }
-    public static int STATE_ROLL { get { return 3; } }
-    public static int STATE_MID_ATTACK { get { return 4; } }
-    public static int STATE_MID_COMBO { get { return 5; } }
-    public static int STATE_JUMP { get { return 6; } }
+	public const int STATE_IDLE = 0;
+	public const int STATE_MOVE_LEFT = 1;
+	public const int STATE_MOVE_RIGHT = 2;
+	public const int STATE_ROLL = 3;
+	public const int STATE_HIGH_ATTACK = 4;
+	public const int STATE_MID_ATTACK = 5;
+	public const int STATE_LOW_ATTACK = 6;
+	public const int STATE_MID_COMBO = 7;
+	public const int STATE_JUMP = 8;
 
-	public static int BASE_MOVE_SPEED { get { return 100; } }
-	public static int BASE_JUMP_SPEED { get { return 250; } }
+	public const int BASE_MOVE_SPEED = 100;
+	public const int BASE_JUMP_SPEED = 250;
+
 }

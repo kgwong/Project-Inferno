03ce091bcb26b31ee14e0471183c05f43f665a8d
cnavasca95 <christian.navasca@gmail.com>
2017-01-27 14:27:16 -0800

    Remove comments

--- Assets/Character/Scripts/PlayerState.cs
+++ Assets/Character/Scripts/PlayerState.cs
@@ -77,35 +77,8 @@ class PlayerState
 		return _animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0;
 	}
 
-	int GetStateHash(int state) // hack
+	int GetStateHash(int state) 
 	{
-        // alternative: make all of the enums named the same as the states
-        // compare to short hash instead of fullpathhash
-        // downside: enums and state names must be typed correctly
-        // downside: enums must be named exactly as the states
-        // upside: don't need this hacky, fragile section
-        // return Animator.StringToHash(state.toString())
-        /*switch (state)
-		{
-			// Animator.fullPathHash needs the "Base Layer." in .StringToHash call
-			case (int)PlayerStateEnum.STATE_IDLE:
-				return Animator.StringToHash("Base Layer.TestIdle");
-			case Constants.STATE_MOVE_LEFT:
-				return Animator.StringToHash("Base Layer.TestMoveLeft");
-			case Constants.STATE_MOVE_RIGHT:
-				return Animator.StringToHash("Base Layer.TestMoveRight");
-			case Constants.STATE_ROLL:
-				return Animator.StringToHash("Base Layer.TestRoll");
-			case Constants.STATE_MID_ATTACK:
-				return Animator.StringToHash("Base Layer.TestMidAttack");
-			case Constants.STATE_MID_COMBO:
-				return Animator.StringToHash("Base Layer.TestMidAttackCombo");
-			case Constants.STATE_JUMP:
-				return Animator.StringToHash("Base Layer.TestJump");
-			default:
-				Debug.Log("STATE \"" + state + "\" IS NOT VALID");
-				return 0;
-		}*/
         string baseLayer = "Base.";
         string stateName = ((PlayerStateEnum)state).ToString();
 		return Animator.StringToHash(baseLayer + stateName);

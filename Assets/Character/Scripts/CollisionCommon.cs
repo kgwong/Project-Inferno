838e6f1b30051408accfde4941220d74c6604a51
cnavasca95 <christian.navasca@gmail.com>
2017-04-16 21:00:00 -0700

    Basic character movement

---
+++ Assets/Character/Scripts/CollisionCommon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class CollisionCommon 
+{
+	public static bool IsGrounded(GameObject go)
+	{
+		float colliderDistToGround = GetActiveCollider(go).bounds.extents.y;
+		const float pad = .1f;
+		RaycastHit2D hit = Physics2D.Raycast(go.transform.position, Vector2.down, colliderDistToGround + pad);
+		return hit.collider != null;
+	}
+
+    public static Collider2D GetActiveCollider(GameObject go)
+    {
+        foreach (Collider2D collider in go.GetComponents<Collider2D>())
+        {
+            if (collider.enabled)
+                return collider;
+        }
+        return null;
+    }
+}
---
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private PlayerStateController _controller;
+	private Rigidbody2D _rb;
+
+	void Start()
+	{
+		_controller = GetComponent<PlayerStateController>();
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate()
+	{
+        int dir = (_controller.FacingRight()) ? 1 : -1;
+		switch (_controller.GetState())
+		{
+			case PlayerStateEnum.TestMove:
+            case PlayerStateEnum.TestAirborneMove:
+				_rb.AddRelativeForce(transform.right * Constants.BASE_MOVE_SPEED * dir);
+				break;
+			case PlayerStateEnum.TestJump:
+				if (CollisionCommon.IsGrounded(gameObject))
+					_rb.AddRelativeForce(transform.up * Constants.BASE_JUMP_SPEED);
+				break;
+			case PlayerStateEnum.TestRoll:
+                _rb.AddRelativeForce(transform.right * Constants.BASE_ROLL_SPEED * dir);
+				break;
+		}
+	}
+}

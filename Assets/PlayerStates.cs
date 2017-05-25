c7cbddc170ff448ca30df4252246a6bf11076957
PaxtonWong <wongpn@uci.edu>
2017-05-24 18:11:38 -0700

    Health and Stamina GUI, main menu GUI, Player Status logic

---
+++ Assets/PlayerStates.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStates : MonoBehaviour {
+
+    Health playerHealth;
+    Stamina playerStamina;
+	// Use this for initialization
+	void Start ()
+    {
+        playerHealth = GetComponent<Health>();
+        playerStamina = GetComponent<Stamina>();
+
+	}
+
+    public void Ondeath ()
+    {
+        //respawn, reset hp/stamina, etc.
+        playerHealth.reset();
+        playerStamina.reset();
+    }
+	
+	// Update is called once per frame
+}

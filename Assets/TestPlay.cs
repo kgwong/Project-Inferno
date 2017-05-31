9b0cad129fb490579c585b8e5a59e98258428d43
PaxtonWong <wongpn@uci.edu>
2017-05-31 16:57:50 -0700

    connected main menu to scene/quit

---
+++ Assets/TestPlay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class TestPlay : MonoBehaviour {
+
+    public void playGame()
+    {
+        print("loading level");
+        SceneManager.LoadScene(1);
+    }
+}

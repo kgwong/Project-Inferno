e6e022cb6f2597aef8dfd62e20dd0c26df87fb04
PaxtonWong <wongpn@uci.edu>
2017-05-31 16:57:50 -0700

    connected main menu to scene/quit

---
+++ Assets/Character/Scripts/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+	// Currently still testing level loading.
+	void Start () {
+        print("loading level");
+        SceneManager.LoadScene(1);
+	
+	}
+	
+}
